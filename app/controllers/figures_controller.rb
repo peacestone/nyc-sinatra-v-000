7be14664cb87a0985c717b609fafd3696649be6d
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-11 17:37:58 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,2 @@
+class FiguresController < Sinatra::Base
+end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,5 +1,5 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  #raise "Write CreateLandmarks migration here"
   create_table :landmarks do |t|
     t.string :name
     t.integer :figure_id
